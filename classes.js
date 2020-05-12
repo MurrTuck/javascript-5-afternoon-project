48c8cbcf4c53d76c06c3b0147651673d6a93676b
MurrTuck <murrtuck@gmail.com>
2020-05-11 19:00:26 -0600

    Classes Done

--- classes.js
+++ classes.js
@@ -31,6 +31,21 @@
 
 //Code Here
 
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
+let newEmployee = new Employee("Murray", "Tucker", "murray@email.com", 34)
+console.log(newEmployee)
+// newEmployee.makeWidget()
 
 ////////// PROBLEM 2 //////////
 
@@ -48,6 +63,19 @@
 */
 
 //Code Here
+
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(employee) {
+    this.reports.push(employee)
+  }
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,6 +100,40 @@
 */
 
 //Code Here
+
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title = 'Not a manager', bonus = 0) {
+    super(first_name, last_name, email, age, reports)
+    this.title = title
+    this.bonus = bonus
+  }
+  titleChange() {
+    let repNum = this.reports.length
+    if (repNum === 0) {
+      this.title = 'Not a manager'
+    } else if (repNum < 4) {
+      this.title = 'Barely Manager'
+    } else if (repNum < 11) {
+      this.title = 'Mostly Manager'
+    } else if (repNum < 51) {
+      this.title = 'Manager'
+    } else if (repNum < 101) {
+      this.title = 'Manager Plus'
+    } else {
+      this.title = 'Bestest Manager'
+    }
+  }
+  hire(employee) {
+    super.hire(employee)
+    this.titleChange()
+  }
+
+  fire(index) {
+    super.fire(index)
+    this.bonus += 100
+    this.titleChange()
+  }
+}
 
 
 
@@ -100,4 +162,26 @@
 
 //Code Here
 
-
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(num) {
+    this.widgets_made_count += num
+    // if (this.widgets_made_count % 50 === 0){
+    //   this.wear_and_tear_count += 1
+    // }
+    this.wear_and_tear_count = Math.floor(this.widgets_made_count / 50)
+  }
+  fixMachine() {
+    this.needs_reboot = true
+  }
+  reboot() {
+    return () => {
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+}
--- closures.js
+++ closures.js
@@ -11,24 +11,25 @@
 // Do not edit the code below.
 function outer() {
   var name = 'Tyler';
-  return function() {
+  return function () {
     return 'The original name was ' + name;
   };
 }
 // Do not edit the code above.
-  
+
 /* 
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
 
+// Code Here
+let inner = outer()
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+inner()
 
 
 
@@ -52,6 +53,9 @@ function callFriend(name) {
 */
 
 //Code Here
+let callJake = callFriend('Jake')
+callJake('435-555-9248')
+
 
 
 
@@ -62,15 +66,20 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter() {
+  let num = 1
+  return function () {
+    return num++
+  }
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+var count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
 
 
@@ -86,9 +95,15 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
+  let counter = value
 
   return {
+    inc: function () {
+      return ++counter
+    },
+    dec: function () {
+      return --counter
+    }
 
   };
 }
@@ -109,10 +124,11 @@ counter = counterFactory(10);
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
 
-function motivation( firstname, lastname ) {
+function motivation(firstname, lastname) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+
 
   //Uncommment this to return the value of your message function
   //return message;
@@ -129,14 +145,14 @@ var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up B
   Invoke this by calling module.publicMethod(); outside the module scope
 */
 
-var module = (function() {
+var module = (function () {
   var person = {
     name: "phillip",
     age: 29,
     location: "Utah"
   };
 
-  function privateMethod(){
+  function privateMethod() {
     return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
   }
 
@@ -169,7 +185,7 @@ function secretNumber() {
 
 
 ////////// PROBLEM 8 //////////
-  
+
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
@@ -188,7 +204,7 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
+    setTimeout(function () {
       console.log(i);
     }, i * 1000);
   }
